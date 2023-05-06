027971f1cc72fbccb80c955adde5643217a42e94
kostia-stepanenko <stepanenko.kotia@gmail.com>
2023-05-06 15:20:38 +0000

    homework 1 queue added

--- BigHomework1/Homework_2_query/Homework_2_query.cpp
+++ BigHomework1/Homework_2_query/Homework_2_query.cpp
@@ -1,4 +1,6 @@
 
+#include <stdexcept>
+#include <string> 
 #include <cstdio>
 #include <iostream>
 #include <queue>
@@ -27,6 +29,22 @@ public:
 	int getExpectedEndTime() {
 		return this->startTime + duration;
 	}
+
+	friend istream& operator>> (istream& in, Order& order) {
+		cout << "Enter Order arrival time: " << flush;
+		cin >> order.startTime;
+		if (order.startTime < 0 || order.startTime > 24 ) {
+			throw invalid_argument("Order 'startTime' should be in range [0; 24]");
+		}
+
+		cout << "Enter Order duration: " << flush;
+		cin >> order.duration;
+		if (order.duration < 1 || order.duration > 24) {
+			throw invalid_argument("Order 'duration' should be in range [1; 24]");
+		}
+
+		return in;
+	}
 };
 
 class Restaurant {
@@ -34,6 +52,17 @@ class Restaurant {
 private:
 	int limit;
 	queue<Order> ordersQueue;
+
+	string convertTime(int time) {
+		if (time <= 24) {
+			return to_string(time);
+		}
+
+		int days = time / 24;
+		int hours = time % 24;
+
+		return to_string(days) + " days " + to_string(hours) + " hours";
+	}
 
 public:
 	Restaurant(int upperLimit) : limit(upperLimit) {}
@@ -74,8 +103,10 @@ public:
 
 				maxOrderDuration = max(maxOrderDuration, curOrder.getDuration());
 
-				printf("Order %d: expected completion time = %d, actual completion time = %d;\n", 
-					orderId, curOrder.getExpectedEndTime(), curTime);
+				printf("Order %d: expected completion time = %s, actual completion time = %s;\n", 
+					orderId, 
+					convertTime(curOrder.getExpectedEndTime()).c_str(), 
+					convertTime(curTime).c_str());
 			}
 
 			++orderId;
@@ -88,6 +119,8 @@ public:
 };
 
 int main(){
+
+	/*
 	int N = 6;
 	int T = 20;
 
@@ -98,10 +131,42 @@ int main(){
 	r.addOrder(Order(10, 3));
 	r.addOrder(Order(11, 2));
 	r.addOrder(Order(12, 4));
-	r.addOrder(Order(18, 5));
+	r.addOrder(Order(18, 10));
 	r.addOrder(Order(20, 4));
 
 	r.execute();
+	*/
+
+	try {
+		cout << "Specify number of orders (a.k.a 'N'): " << flush;
+		int N;
+		cin >> N;
+		if (N < 0 || N > 100) {
+			throw invalid_argument("'N' should be in range [0; 100]");
+		}
+
+		cout << "Specify UPPER time limit for restaurant(a.k.a T): " << flush;
+		int T;
+		cin >> T;
+		if (T < 0 || T > 24) {
+			throw invalid_argument("'T' should be in range [0; 24]");
+		}
+
+		Restaurant r(T);
+
+		for (int i = 0; i < N; ++i) {
+			// read Order from cin
+			cout << "Order " << i << ":" << endl;
+			Order cur(0, 0);
+			cin >> cur;
+			r.addOrder(cur);
+		}
+
+		r.execute();
+	}
+	catch (const exception& ex) {
+		cerr << ex.what() << endl;
+	}
 
 	cout << "homework_1_queue completed" << endl;
 	return 0;
