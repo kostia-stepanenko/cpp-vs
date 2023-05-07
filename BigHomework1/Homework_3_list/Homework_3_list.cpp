368191b55290719ed28df1ca6d0860000e906d1a
kostia-stepanenko <stepanenko.kotia@gmail.com>
2023-05-07 10:50:27 +0000

    add for polynomials added

--- BigHomework1/Homework_3_list/Homework_3_list.cpp
+++ BigHomework1/Homework_3_list/Homework_3_list.cpp
@@ -15,6 +15,11 @@ private:
 
 public:
 
+    Term(const Term& other) {
+        this->coeff = other.coeff;
+        this->exp = other.exp;
+    }
+
     Term(double coeff, int exp) {
         this->coeff = coeff;
         this->exp = exp;
@@ -72,6 +77,47 @@ public:
         return res;
     }
 
+    Polynomial sum(Polynomial& second) {
+        Polynomial res;
+
+        auto firstIt = this->terms.begin();
+        auto secondIt = second.terms.begin();
+
+        while (firstIt != this->terms.end() || secondIt != second.terms.end()) {
+
+            // exp1 == exp2
+            if (firstIt->getExp() == secondIt->getExp()) {
+
+                double curCoeff = firstIt->getCoeff() + secondIt->getCoeff();
+
+                res.addTerm(Term(curCoeff, firstIt->getExp()));
+
+                ++firstIt;
+                ++secondIt;
+            }
+
+            // exp1 > exp2
+            else if (firstIt->getExp() > secondIt->getExp()) {
+                res.addTerm(Term(*firstIt));
+                ++firstIt;
+            }
+
+            // exp1 < exp2
+            else {
+                res.addTerm(Term(*secondIt));
+                ++secondIt;
+            }
+        }
+
+        return res;
+    }
+
+    Polynomial mul(Polynomial& second) {
+        Polynomial res;
+
+        return res;
+    }
+
     friend ostream& operator<<(ostream& os, const Polynomial& p) {
 
         int index = 0;
@@ -100,16 +146,31 @@ public:
 
 int main(){
 
+    Polynomial p1;
+    p1.addTerm(Term(-3.15, 5));
+    p1.addTerm(Term(12, 4));
+    p1.addTerm(Term(0, 2));
+    p1.addTerm(Term(-18, 1));
+    p1.addTerm(Term(13, 0));
 
-    Polynomial p;
-    p.addTerm(Term(-3.15, 5));
-    p.addTerm(Term(12, 4));
-    p.addTerm(Term(0, 2));
-    p.addTerm(Term(-18, 1));
-    p.addTerm(Term(13, 0));
+    cout << "p1: " << p1 << endl;
 
+    cout << "p(X) = " << p1.evaluate(12.34) << endl;
 
-    cout << p << endl;
+    Polynomial p2;
+    p2.addTerm(Term(14.56, 8));
+    p2.addTerm(Term(10, 4));
+    p2.addTerm(Term(13, 1));
+    p2.addTerm(Term(17, 0));
+
+    Polynomial sumRes = p1.sum(p2);
+
+    cout << "p2: " << p2 << endl;
+    cout << "p1 + p1: " << sumRes << endl;
+
+    Polynomial prodRes = p1.mul(p2);
+
+    cout << "p1 * p2: " << prodRes << endl;
 
     cout << "homework 3 polinoms as list done" << endl;
 
